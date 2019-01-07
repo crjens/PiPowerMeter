d393ef05063db5b5ca379921c2ef20da33d908d2
Craig Jensen <cjensen@microsoft.com>
2019-01-06 19:53:14 -0800

    update

--- setup.sh
+++ setup.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
+
+# check if sudo is used
+if [ "$(id -u)" != 0 ]; then
+  echo 'Sorry, you need to run this script with sudo'
+  exit 1
+fi
+
 cd ~
-curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
-sudo apt install -y nodejs
-sudo apt-get -y install git
-git clone https://github.com/crjens/PiPowerMeter.git app
+#curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+#sudo apt install -y nodejs
+#sudo apt-get -y install git
+#git clone https://github.com/crjens/PiPowerMeter.git app
 cd app
-git checkout test
-git pull
-npm install
+#git checkout test
+#git pull
+#npm install
+ 
+# enable SPI on Raspberry Pi
+echo '>>> Enable SPI'
+if grep -q 'dtparam=spi=on' /boot/config.txt; then
+  echo 'SPI already enab;ed'
+else
+  echo 'dtparam=spi=on' >> /boot/config.txt
+fi
