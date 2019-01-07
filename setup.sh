6129a0c82e94cfb512e6e980609d7d1375db0f88
Craig Jensen <cjensen@microsoft.com>
2019-01-06 20:57:27 -0800

    fix path

--- setup.sh
+++ setup.sh
@@ -6,15 +6,14 @@ if [ "$(id -u)" != 0 ]; then
   exit 1
 fi
 
-cd ~
-#curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
-#sudo apt install -y nodejs
-#sudo apt-get -y install git
-#git clone https://github.com/crjens/PiPowerMeter.git app
+curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
+sudo apt install -y nodejs
+sudo apt-get -y install git
+git clone https://github.com/crjens/PiPowerMeter.git app
 cd app
-#git checkout test
-#git pull
-#npm install
+git checkout test
+git pull
+npm install
  
 # enable SPI on Raspberry Pi
 echo '>>> Enable SPI'
