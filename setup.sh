6cf14290e3b8afa8b92b5cf72eadb5257afe0bb7
Craig Jensen <cjensen@microsoft.com>
2019-01-06 21:18:10 -0800

    updates

--- setup.sh
+++ setup.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 
-# check if sudo is used
-if [ "$(id -u)" != 0 ]; then
-  echo 'Sorry, you need to run this script with sudo'
-  exit 1
-fi
-
+# install nodejs
 curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
 sudo apt install -y nodejs
+
+# install git
 sudo apt-get -y install git
+
+# install and configure PiPowerMeter
 git clone https://github.com/crjens/PiPowerMeter.git app
 cd app
 git checkout test
 git pull
 npm install
  
-# enable SPI on Raspberry Pi
+# enable SPI
 echo '>>> Enable SPI'
 sudo sed -i 's/^#dtparam=spi=on.*//' /boot/config.txt
 if grep -q 'dtparam=spi=on' /boot/config.txt; then
@@ -25,7 +24,7 @@ else
   echo 'SPI enabled'
 fi
 
-# enable UART on Raspberry Pi
+# enable UART
 echo '>>> Enable UART'
 wget -O uart_control.sh https://raw.githubusercontent.com/itemir/rpi_boat_utils/master/uart_control/uart_control
 chmod +x uart_control.sh
