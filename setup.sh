64b7811cb8bec7ad7b5ccd9e9468acd26d3f330e
Craig Jensen <cjensen@microsoft.com>
2019-01-20 11:45:51 -0800

    update install script

--- setup.sh
+++ setup.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
 
-# install nodejs on armv6
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
+# require root
+if [ $(id -u) -ne 0 ]; then
+  printf "Script must be run as root. Try 'sudo $0'\n"
+  exit 1
+fi
+
+# update OS
+echo '>>> Update OS Image'
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# install nodejs via nvm
+echo '>>> Install NodeJs'
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
 sudo cp -R $NVM_DIR/versions/node/$(nvm version)/* /usr/local/
 
-# install nodejs
-#curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
-#sudo apt install -y nodejs
-
 # install git
+echo '>>> Install Git'
 sudo apt-get -y install git
 
 # install and configure PiPowerMeter
+echo '>>> Install PiPowerMeter'
 git clone https://github.com/crjens/PiPowerMeter.git app
 cd app
 git checkout test
@@ -23,19 +33,8 @@ git pull
 npm install
  
 # expand filesystem
+echo '>>> Expand FileSystem'
 sudo raspi-config nonint do_expand_rootfs
 
-# enable SPI
-#echo '>>> Enable SPI'
-#sudo raspi-config nonint do_spi 0
-#sudo sed -i 's/^#dtparam=spi=on.*/dtparam=spi=on/' /boot/config.txt
-
-# enable UART
-#echo '>>> Enable UART'
-#sudo raspi-config nonint do_serial 2
-
-#echo '>>> Disable bluetooth and enable PL011 uart'
-#sudo raspi-config nonint set_config_var dtoverlay pi3-disable-bt /boot/config.txt
-#sudo systemctl disable hciuart
-echo '>>> PiPowerMeter is installed.  Please reboot now.'
+echo '>>> PiPowerMeter is installed, restarting...'
 sudo reboot
