50ae15bb6541defc1788f6e18dc9cf49f15a8989
Craig Jensen <cjensen@microsoft.com>
2019-11-07 20:36:15 -0800

    log

--- driver.js
+++ driver.js
@@ -734,6 +734,7 @@ var exports = {
         return 0;
     },
     ReplaceConfiguration: function (callback, config) {
+        console.log("ReplaceConfiguration")
         db.updateCircuits(config, function (err) {
             //loadConfiguration();
             console.log("calling stop")
